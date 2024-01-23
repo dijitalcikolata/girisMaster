ad7e8c56ffe5a5659efc8cdc1dffd7dd5f62e349
dijitalcikolata <dijitalcikolata@icloud.com>
2024-01-23 22:33:08 +0300

    Initial Commit

---
+++ girisMaster/girisMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  girisMasterApp.swift
+//  girisMaster
+//
+//  Created by MURAT HASIRCI on 23.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct girisMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
