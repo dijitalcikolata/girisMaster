25257d676abca8b240c8e760337ec2fd036ccaa5
dijitalcikolata <dijitalcikolata@icloud.com>
2024-01-30 00:18:52 +0300

    master

---
+++ girisMaster/denek.swift
@@ -0,0 +1,18 @@
+//
+//  denek.swift
+//  girisMaster
+//
+//  Created by MURAT HASIRCI on 25.01.2024.
+//
+
+import SwiftUI
+
+struct denek: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    denek()
+}
