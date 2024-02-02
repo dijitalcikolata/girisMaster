82d37614e58c20a978301ad3ff88198bb7261b74
dijitalcikolata <dijitalcikolata@icloud.com>
2024-02-03 01:15:53 +0300

    apple

--- girisMaster/Giris/LoginViewModel.swift
+++ girisMaster/Giris/LoginViewModel.swift
@@ -1,18 +1,18 @@
 //
-//  Kayit.swift
+//  LoginViewModel.swift
 //  girisMaster
 //
-//  Created by MURAT HASIRCI on 2.02.2024.
+//  Created by MURAT HASIRCI on 3.02.2024.
 //
 
 import SwiftUI
 
-struct Kayit: View {
+struct LoginViewModel: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    Kayit()
+    LoginViewModel()
 }
