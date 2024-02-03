82fdb0db92619ebb02e376db6559a48f36dd9115
dijitalcikolata <dijitalcikolata@icloud.com>
2024-02-04 00:06:43 +0300

    f

---
+++ girisMaster/Giris/AuthenticationViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationViewModel.swift
+//  girisMaster
+//
+//  Created by MURAT HASIRCI on 4.02.2024.
+//
+
+import SwiftUI
+
+struct AuthenticationViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationViewModel()
+}
